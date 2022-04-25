db27b4f6488faac32d38a80dd3167718cf63a693
tuannn0107 <tuannn0107@gmail.com>
2022-04-25 09:32:35 +0700

    bean test

--- src/test/java/com/study/springframework/aop/pointcut/PointCutConfigTest.java
+++ src/test/java/com/study/springframework/aop/pointcut/PointCutConfigTest.java
@@ -21,7 +21,7 @@ public class PointCutConfigTest extends AbstractSpringFrameworkStudyTest {
 
         iService.insertNew("abc");
         Assert.assertNotNull(PrintStreamStorage.getInstance().getPrintStreamStorage().get("Call insertNew(String str)."));
-        Assert.assertTrue(PrintStreamStorage.getInstance().getPrintStreamStorage().get("Execute before method insertNew xxx xxx xxx") < PrintStreamStorage.getInstance().getPrintStreamStorage().get("Call insertNew(String str)."));
+        Assert.assertTrue(PrintStreamStorage.getInstance().getPrintStreamStorage().get("Execute before method insertNew xxx xxx xxx ffff") < PrintStreamStorage.getInstance().getPrintStreamStorage().get("Call insertNew(String str)."));
         Assert.assertTrue(PrintStreamStorage.getInstance().getPrintStreamStorage().get("Execute after method insertNew") > PrintStreamStorage.getInstance().getPrintStreamStorage().get("Call insertNew(String str)."));
     }
 }
